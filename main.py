273e969a4a815c907733ed67dd0a0eda02001554
RavaszJan <156021333+RavaszJan@users.noreply.github.com>
2024-02-20 15:48:17 +0100

    ???

--- main.py
+++ main.py
@@ -1,2 +1,28 @@
 # Topanky2
 
+# if __name__=="__main__"
+from topankycontroler import ShoeController
+from topankymodel import ShoeModel
+from topankyview import Shoeview
+
+
+model=ShoeModel()
+view=Shoeview()
+controller=ShoeController(model,view)
+
+
+
+
+    controller.add_shoe(1,"Panske","Nike Air MAx","Modra",43,"Nike",140)
+    shoe_controller.add_shoe(2,"Zenske","Nike Air MAx","Fialova",39,"Nike",145)
+
+    shoe_controller.show_shoes()
+    shoe_controller.show_shoes_size(39)
+    shoe_controller.show_total_price()
+    shoe_controller.remove_shoe(1)
+
+controller.display_shoes()
+
+
+
+
